342b37e99079ab1d7c9ba26e535c7c7ee1e3a053
Fasil11 <seifufasil@gmail.com>
2023-01-04 11:45:13 -0800

    task7

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-def magic_calculation(a, b, c):
+# 6-print_comb3.py
 
+"""Print all possible different combinations of two digits in ascending order.
 
-    """Match bytecode provided by Holberton School."""
-    if a < b:
-        return (c)
-    if c > b:
-        return (a + b)
-        return (a*b - c)
+    The two digits must be different - 01 and 10 are considered identical.
+
+        """
+
+for digit1 in range(0, 10):
+    for digit2 in range(digit1 + 1, 10):
+        if digit1 == 8 and digit2 == 9:
+            print("{}{}".format(digit1, digit2))
+        else:
+            print("{}{}".format(digit1, digit2), end=", ")
