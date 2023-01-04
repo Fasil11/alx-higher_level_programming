6e59272ea5d627ff11758e5fb2a8ce50bc5cd694
Fasil11 <seifufasil@gmail.com>
2023-01-04 11:49:11 -0800

    task8

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# 7-islower.py
+
+def islower(c):
+
+    """Check for lowercase characters."""
+
+            if ord(c) >= 97 and ord(c) <= 122:
+
+                return True
+
+            else:
+
+                return False
+
+            Footer
+
+                                © 2023 GitHub, Inc.
