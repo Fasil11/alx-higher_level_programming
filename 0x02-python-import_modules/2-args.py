b21dd3e5d6286842cc6af417c2ccd63ecade36dc
Fasil11 <seifufasil@gmail.com>
2023-01-05 06:30:53 -0800

    task3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -11,8 +11,7 @@ if __name__ == "__main__":
         print("{:d} arguments.".format(argv_count))
     elif argv_count is 1:
         print("{:d} argument:".format(argv_count))
-        print("{:d}: {:s}".format(index, sys.argv[1]))
-        print("{:d} arguments:".format(argv_count))
+        print("{:d}: {:s}".format(index, sys.argv[1]))   
     else:
         print("{:d} arguments:".format(argv_count))
         while index <= argv_count:
