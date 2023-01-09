fdbead98dd8f9f25434e2ea522cb44f1cd36256a
Fasil11 <seifufasil@gmail.com>
2023-01-08 21:29:37 -0800

    task0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def print_list_integer(my_list):
-    for items in my_list:
+def print_list_integer(my_list=[]):
+    for number in my_list:
         if isinstance(item,, int):
-            print("{}".format(item))
+            print("{:d}".format(number))
