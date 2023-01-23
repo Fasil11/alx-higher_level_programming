06b120b99cd9bbbad873deba170b01433c9b7651
Fasil11 <seifufasil@gmail.com>
2023-01-23 12:11:58 -0800

    tas

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,15 +2,15 @@
 
 
 def safe_print_list(my_list=[], x=0):
-
     i = 0
     printed = 0
+
     for i in range(0, x):
-        try:
+
+       try:
             print("{}".format(my_list[i]), end="")
             printed += 1
-        except:
+       except:
             continue
-
 print()
 return printed
