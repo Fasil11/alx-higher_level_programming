67f4acfbcf876c2b6f8edc60fce5ba8f9d16b602
Fasu <113653474+Fasil11@users.noreply.github.com>
2023-01-23 12:05:40 -0800

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,6 @@
 
 
 def safe_print_list(my_list=[], x=0):
-
     i = 0
     printed = 0
     for i in range(0, x):
@@ -11,6 +10,5 @@ def safe_print_list(my_list=[], x=0):
             printed += 1
         except:
             continue
-
-print()
-return printed
+    print()
+    return printed
