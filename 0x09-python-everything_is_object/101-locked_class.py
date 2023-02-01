b8f6a3ab2ac0c101cce6cafc5cf5d2ec9d73812d
Fasil11 <seifufasil@gmail.com>
2023-02-01 03:48:15 -0800

    task

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,8 +2,11 @@
 
 """Defines a class LockedClass"""
 
-class LockedClass:
-    __slots__ = ["first_name"]
-     def __init__(self):
 
-         self.first_name = "first_name"
+class LockedClass:
+
+    __slots__ = ["first_name"]
+
+    def __init__(self):
+
+        self.first_name = "first_name"
