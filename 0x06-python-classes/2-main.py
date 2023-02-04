3db766e505cffd10f7fea60602db08d6422cb508
Fasil11 <seifufasil@gmail.com>
2023-02-04 09:50:17 +0000

    tasks

--- 0x06-python-classes/2-main.py
+++ 0x06-python-classes/2-main.py
@@ -9,13 +9,11 @@ print(type(my_square_1))
 print(my_square_1.__dict__)
 
 
-
 my_square_2 = Square()
 
 print(type(my_square_2))
 
 print(my_square_2.__dict__)
-
 
 try:
     print(my_square_1.size)
