bfe7f79a3ae32ca30c89406aa5a48cf4657cd24c
Fasil11 <seifufasil@gmail.com>
2023-04-10 12:46:57 -0700

    tasks

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -3,7 +3,7 @@
 // prints 3 lines: (like 1-multi_languages.js) but by
 // using an array of string and a loop
 
-const lines = ["C is fun", "Python is cool", "Javascript is amazing"]:
+const lines = ["C is fun", "Python is cool", "Javascript is amazing"];
 
 for (let i = 0; i < lines.length; i++) {
 	console.log(lines[i]);
