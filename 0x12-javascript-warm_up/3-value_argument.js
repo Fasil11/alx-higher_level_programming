c1ae1aa65e4583078dd84d2719183a3f5a1ba79e
Fasil11 <seifufasil@gmail.com>
2023-04-12 03:15:54 -0700

    tasks

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,2 @@
 #!/usr/bin/node
-
-const arg = process.argv[2];
-
-if (arg) {
-	console.log(arg);
-} else {
-	console.log("No argument");
-}
+console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
