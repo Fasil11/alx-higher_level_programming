526cf5c24b462498b14dcbbf0ce8e76e9343dd40
Fasil11 <seifufasil@gmail.com>
2023-04-12 06:28:52 -0700

    tasks

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,14 +1,10 @@
 #!/usr/bin/node
 
-const arg = process.argv[2];
-if (isNaN(parseInt(arg)))
-{
-	console.log("Missing number of occurrences");
-}
-else {
-	const x = parseInt(arg);
-	let i = 0;
-	while (i < x)
-	{ console.log("C is fun");
-	i++; }
+const x = Math.floor(Number(process.argv[2]));
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
 }
