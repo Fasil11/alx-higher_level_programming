c4a0fd7b150b11c830a3267c23146a6358e91687
Fasil11 <seifufasil@gmail.com>
2023-04-12 03:35:47 -0700

    tasks

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,14 @@
 #!/usr/bin/node
 
-const name = process.argv[2]
-
-if (!name) {
-    console.log("please provide a name");
-} else {
-    console.log('Hello, ${name}');
+const args = process.argv.slice(2);
+if (args.length === 0)
+{
+	console.log("No argument"); 
+}
+else if (args.length === 1)
+{
+	console.log("Argument found");
+}
+else {
+	console.log("Arguments found");
 }
